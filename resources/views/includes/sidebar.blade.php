b6acaf053241682dc85a2754e66c790fc0ecb31a
omichsam <omichsam@gmail.com>
2025-04-27 12:24:37 +0300

    refactor: reorganize navbar menu links for improved structure and usability; enhance navigation flow

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -22,17 +22,18 @@
         </p>
         <ul class="navbar-nav flex-fill w-100 mb-2">
             <li class="nav-item w-100">
-                <a class="nav-link" href="{{ route('patient.index') }}">
-                    <i class="fe fe-users fe-16"></i>
-                    <span class="ml-3 item-text">Patients</span>
-                </a>
-            </li>
-            <li class="nav-item w-100">
                 <a class="nav-link" href="{{ route('program.index') }}">
                     <i class="fe fe-hard-drive fe-16"></i>
                     <span class="ml-3 item-text">Programs</span>
                 </a>
             </li>
+            <li class="nav-item w-100">
+                <a class="nav-link" href="{{ route('patient.index') }}">
+                    <i class="fe fe-users fe-16"></i>
+                    <span class="ml-3 item-text">Patients</span>
+                </a>
+            </li>
+            
         </ul>
     </nav>
 </aside>
